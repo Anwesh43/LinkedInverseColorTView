eb731d0fbfbd73800d5dcdd2feb18bad4a6a1a06
Anwesh43 <anweshthecool0@gmail.com>
2020-06-24 16:24:30 +0530

    adding ICTNode class

--- inversecolortview/src/main/java/com/anwesh/uiprojects/inversecolortview/InverseColorTView.kt
+++ inversecolortview/src/main/java/com/anwesh/uiprojects/inversecolortview/InverseColorTView.kt
@@ -114,4 +114,45 @@ class InverseColorTView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ICTNode(var i : Int, val state : State = State()) {
+
+        private var next : ICTNode? = null
+        private var prev : ICTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ICTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawICTNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ICTNode {
+            var curr : ICTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
