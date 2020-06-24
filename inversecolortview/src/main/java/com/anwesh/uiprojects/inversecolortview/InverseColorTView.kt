679f84c2142257ebf7d01db6563d591c09eafc0b
Anwesh43 <anweshthecool0@gmail.com>
2020-06-24 16:10:30 +0530

    adding Canvas extension methods

--- inversecolortview/src/main/java/com/anwesh/uiprojects/inversecolortview/InverseColorTView.kt
+++ inversecolortview/src/main/java/com/anwesh/uiprojects/inversecolortview/InverseColorTView.kt
@@ -23,3 +23,31 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawInverseColorT(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = w / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, 2)
+    val sf2 : Float = sf.divideScale(1, 2)
+    drawRect(RectF(-size, 0f, size, h * sf1), paint)
+    for (j in 0..1) {
+        save()
+        translate(0f, h)
+        scale(1f - 2 * j, 1f)
+        save()
+        translate(size, 0f)
+        drawRect(RectF(0f, -size, (w / 2 - size) * sf2, 0f), paint)
+        restore()
+        restore()
+    }
+}
+
+fun Canvas.drawICTNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    save()
+    translate(w / 2, 0f)
+    drawInverseColorT(scale, w, h, paint)
+    restore()
+}
