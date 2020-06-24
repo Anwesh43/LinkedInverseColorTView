eaa978d2b2e82b68805a0de2f5066eecff2bca51
Anwesh43 <anweshthecool0@gmail.com>
2020-06-24 16:40:55 +0530

    adding renderer in view

--- inversecolortview/src/main/java/com/anwesh/uiprojects/inversecolortview/InverseColorTView.kt
+++ inversecolortview/src/main/java/com/anwesh/uiprojects/inversecolortview/InverseColorTView.kt
@@ -54,14 +54,16 @@ fun Canvas.drawICTNode(i : Int, scale : Float, paint : Paint) {
 
 class InverseColorTView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
