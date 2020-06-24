f49bf974e506140277beb5b403c1930e62fb2f69
Anwesh43 <anweshthecool0@gmail.com>
2020-06-24 16:14:08 +0530

    adding InverseColorTView class

--- inversecolortview/src/main/java/com/anwesh/uiprojects/inversecolortview/InverseColorTView.kt
+++ inversecolortview/src/main/java/com/anwesh/uiprojects/inversecolortview/InverseColorTView.kt
@@ -51,3 +51,19 @@ fun Canvas.drawICTNode(i : Int, scale : Float, paint : Paint) {
     drawInverseColorT(scale, w, h, paint)
     restore()
 }
+
+class InverseColorTView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
