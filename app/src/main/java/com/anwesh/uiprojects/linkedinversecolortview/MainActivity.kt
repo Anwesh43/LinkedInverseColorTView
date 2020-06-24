c1564047956fa458431706044a1448624039ea3d
Anwesh43 <anweshthecool0@gmail.com>
2020-06-24 16:58:42 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedinversecolortview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedinversecolortview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedinversecolortview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.inversecolortview.InverseColorTView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        InverseColorTView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
